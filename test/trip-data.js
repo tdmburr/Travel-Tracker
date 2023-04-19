75d284d70f764dd8f65b117ea41dee355bfd6558
Trey Marshall <marshall.trey92@yahoo.com>
2023-04-19 16:41:28 -0500

    test: Add trip test data

---
+++ test/trip-data.js
@@ -0,0 +1,32 @@
+const tripsData = [
+  {
+    "id":1,
+    "userID":44,
+    "destinationID":49,
+    "travelers":1,
+    "date":"2022/09/16",
+    "duration":8,
+    "status":"approved",
+    "suggestedActivities":[]
+  },
+  {
+    "id":2,
+    "userID":35,
+    "destinationID":25,
+    "travelers":5,
+    "date":"2022/10/04",
+    "duration":18,
+    "status":"approved",
+    "suggestedActivities":[]
+  },
+  {
+    "id":3,
+    "userID":3,
+    "destinationID":22,
+    "travelers":4,
+    "date":"2022/05/22",
+    "duration":17,
+    "status":"approved",
+    "suggestedActivities":[]
+  }
+]
